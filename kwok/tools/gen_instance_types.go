f8e9aeab0e219cc31e983130cce65092339c8d87
KaykyFidelis <kayky.serafim@lsd.ufcg.edu.br>
2026-04-17 09:33:33 -0300

    chore: updating instance types with new aws families

--- kwok/tools/gen_instance_types.go
+++ kwok/tools/gen_instance_types.go
@@ -20,6 +20,7 @@ var (
 		"m7i-flex", "r5a", "r6g", "r7g", "vt1", "c3", "c5", "c8g", "c5a", "c5ad", "c5d", 
 		"c5n", "c6a", "c6g", "c6gn", "c6i", "c7g", "i3en", "m5a", "m6a",  "m6g", "r5",
 		"t3", "a1", "x2gd", "r8g", "m7g", "m6gd", "hpc7g", "t4g", "c7i", "m6i", "m7i",
+		"c7a", "m7a",
 	}
 
 	InstanceSizes = []string{
@@ -422,6 +423,32 @@ func getCPUMemoryForInstance(family, size string) (int, int, int, int) {
 		case "large": return 2, 8, 50, 32
 		case "xlarge": return 4, 16, 100, 64
 		case "2xlarge": return 8, 32, 100, 128
+		}
+	
+
+	case "c7a":
+		switch size {
+		case "2xlarge": return 8, 16, 100, 128
+		case "4xlarge": return 16, 32, 200, 256
+		case "8xlarge": return 32, 64, 400, 512
+		case "12xlarge": return 48, 96, 600, 768
+		case "16xlarge": return 64, 128, 800, 1024
+		case "24xlarge": return 96, 192, 1200, 1536
+		case "large": return 2, 4, 50, 32
+		case "xlarge": return 4, 8, 100, 64
+    	}
+
+	case "m7a":
+		switch size {
+		case "2xlarge": return 8, 32, 100, 128
+		case "4xlarge": return 16, 64, 200, 256
+		case "8xlarge": return 32, 128, 400, 512
+		case "12xlarge": return 48, 192, 600, 768
+		case "16xlarge": return 64, 256, 800, 1024
+		case "24xlarge": return 96, 384, 1200, 1536
+		case "32xlarge": return 128, 512, 1600, 2048
+		case "large": return 2, 8, 50, 32
+		case "xlarge": return 4, 16, 100, 64
 		}
 	}
 	return 0, 0, 0, 0
